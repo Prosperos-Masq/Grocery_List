0b99dde195a6668e0d718dbfbb1dd2e65fcd1884
Prospero <60527636+Prosperos-Masq@users.noreply.github.com>
2021-07-17 23:08:37 -0500

    Created a Grocery List
    Made a grocery list using html, css, and javascript to be able to add and remove items from the list

---
+++ Grocery_List.js
@@ -0,0 +1,18 @@
+const add = document.querySelector('.add');
+const remove = document.querySelector('.remove');
+const input = document.querySelector('.info');
+
+
+add.addEventListener('click', () => {
+  let ul = document.querySelector('ul');
+  let li = document.createElement('li');
+  li.textContent = input.value;
+  ul.appendChild(li);
+  input.value = '';
+});
+
+remove.addEventListener('click', () => {
+  let ul = document.querySelector('ul');
+  let li = document.querySelector('li:last-child');
+  ul.removeChild(li);
+});
