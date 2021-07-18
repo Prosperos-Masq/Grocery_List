05706a677345757bef9df5cfd608d3e08ed876f2
Prospero <60527636+Prosperos-Masq@users.noreply.github.com>
2021-07-18 17:47:20 -0500

    javascript update
    added a scroll bar in css and testing code in js

--- Grocery_List.js
+++ Grocery_List.js
@@ -16,3 +16,23 @@ remove.addEventListener('click', () => {
   let li = document.querySelector('li:last-child');
   ul.removeChild(li);
 });
+
+// add.addEventListener('click', () => {
+//   if (!!strValue) {
+//     alert('Please enter something!')
+//   } else {
+//     let ul = document.querySelector('ul');
+//     let li = document.createElement('li');
+//     li.textContent = input.value;
+//     ul.appendChild(li);
+//     input.value = '';
+//   }
+// });
+
+// remove.addEventListener('click', () => {
+//   if ()
+
+//   let ul = document.querySelector('ul');
+//   let li = document.querySelector('li:last-child');
+//   ul.removeChild(li);
+// });
